5f7af853117c065f99bf636c24536db9b90cc8cb
WesleyDev184 <wa.wesley01@gmail.com>
2025-10-21 21:33:31 -0300

    att

---
+++ apps/api-gateway/src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import 'dotenv/config';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  console.log(`API is running on http://localhost:${port}`);
+}
+
+void bootstrap();
