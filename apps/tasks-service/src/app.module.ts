cb9ac2e2389b8e82f68f3cda5f4e4d6d45332293
WesleyDev184 <wa.wesley01@gmail.com>
2025-10-24 06:31:28 -0300

    att

--- apps/tasks-service/src/app.module.ts
+++ apps/tasks-service/src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+
 import { DatabaseModule } from './shared/database/database.module'
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
